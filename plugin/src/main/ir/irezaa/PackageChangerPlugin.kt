331fc50ce03aedc9028ec52e1418c32081c0d68e
Reza Pilehvar <reza@snapps-MacBook-Pro.local>
2021-10-13 17:55:53 +0330

    init

---
+++ plugin/src/main/ir/irezaa/PackageChangerPlugin.kt
@@ -0,0 +1,10 @@
+package ir.irezaa
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class PackageChangerPlugin : Plugin<Project> {
+    override fun apply(p0: Project) {
+        TODO("Not yet implemented")
+    }
+}
